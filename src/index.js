a6973913582543ec9094f55b4500f77583e46d11
Rothana Choun <rothana.choun7@gmail.com>
2020-02-18 21:16:49 +0700

    chore(): upgrade react native webview

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import { WebView, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
+import { WebView } from "react-native-webview";
 
-import katexStyle from './katex-style';
-import katexScript from './katex-script';
-import { bool, func, object, string } from 'prop-types';
+import katexStyle from "./katex-style";
+import katexScript from "./katex-script";
+import { bool, func, object, string } from "prop-types";
 
-function getContent({ inlineStyle, expression = '', ...options }) {
+function getContent({ inlineStyle, expression = "", ...options }) {
   return `<!DOCTYPE html>
 <html>
 <head>
@@ -15,7 +16,9 @@ ${inlineStyle}
 </style>
 <script>
 window.onerror = e => document.write(e);
-window.onload = () => katex.render(${JSON.stringify(expression)}, document.body, ${JSON.stringify(options)});
+window.onload = () => katex.render(${JSON.stringify(
+    expression
+  )}, document.body, ${JSON.stringify(options)});
 ${katexScript}
 </script>
 </head>
@@ -27,7 +30,7 @@ ${katexScript}
 
 const defaultStyle = StyleSheet.create({
   root: {
-    height: 40,
+    height: 40
   }
 });
 
@@ -47,7 +50,6 @@ html, body {
 `;
 
 export default class Katex extends Component {
-
   static propTypes = {
     expression: string.isRequired,
     displayMode: bool,
@@ -57,30 +59,32 @@ export default class Katex extends Component {
     macros: object,
     colorIsTextColor: bool,
     onLoad: func,
-    onError: func,
+    onError: func
   };
 
   static defaultProps = {
     displayMode: false,
     throwOnError: false,
-    errorColor: '#f00',
+    errorColor: "#f00",
     inlineStyle: defaultInlineStyle,
     style: defaultStyle,
     macros: {},
     colorIsTextColor: false,
     onLoad: () => {},
-    onError: () => {},
+    onError: () => {}
   };
 
   render() {
     const { style, onLoad, onError, ...options } = this.props;
 
-    return <WebView
-      style={style}
-      source={{ html: getContent(options) }}
-      onLoad={onLoad}
-      onError={onError}
-      renderError={onError}
-    />;
+    return (
+      <WebView
+        style={style}
+        source={{ html: getContent(options) }}
+        onLoad={onLoad}
+        onError={onError}
+        renderError={onError}
+      />
+    );
   }
 }
